2c993a4683f90054fb142b6e04cd37a11b0fabeb
luzzhong <979126035@qq.com>
2024-08-08 16:28:27 +0800

    新增异兽入侵

--- src/game/common/MsgRecvMgr.js
+++ src/game/common/MsgRecvMgr.js
@@ -21,6 +21,7 @@ import HeroRankMgr from "#game/mgr/HeroRankMgr.js";
 import ActivityMgr from "#game/mgr/ActivityMgr.js";
 import UnionMgr from "#game/mgr/UnionMgr.js";
 import HomelandMgr from "#game/mgr/HomelandMgr.js";
+import InvadeMgr from "#game/mgr/InvadeMgr.js";
 
 class MsgRecvMgr {
     constructor() {
@@ -316,6 +317,15 @@ class MsgRecvMgr {
         HomelandMgr.inst.doExplore(t);
     }
 
+     // 1402 异兽入侵数据同步
+    static InvadeDataMsg(t) {
+        logger.debug("[MsgRecvMgr] 异兽入侵挑战");
+        InvadeMgr.inst.InvadeDataMsg(t);
+    }
+    static InvadeChallengeResp(t) {
+        logger.debug("[MsgRecvMgr] 异兽入侵用户数据同步");
+        InvadeMgr.inst.InvadeChallengeResp(t);
+    }
 // TODO: 以下代码未完成
 // import SystemUnlockMgr from "#game/mgr/SystemUnlockMgr.js";
 //     // 102 系统解锁同步
@@ -332,13 +342,6 @@ class MsgRecvMgr {
 //     }
 
 // TODO 以下暂时不想写
-// import InvadeMgr from "#game/mgr/InvadeMgr.js";
-//     // 1402 异兽入侵数据同步
-//     static InvadeDataMsg(t) {
-//         logger.debug("[MsgRecvMgr] 异兽入侵用户数据同步");
-//         InvadeMgr.inst.InvadeDataMsg(t);
-//         // 5次
-//     }
 
 
 // import CommonRedPacketMgr from "#game/mgr/CommonRedPacketMgr.js";
